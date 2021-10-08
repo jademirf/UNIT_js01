bb57d816c64ef3013139542a0a04ed39b886b310
Jademir de Moura <jademirf@gmail.com>
2021-10-08 20:30:20 -0300

    mapeando botao e adicionando o eventListenner

--- js/global.js
+++ js/global.js
@@ -1,5 +1,13 @@
 //imagem da lâmpada acesa
 // http://i.stack.imgur.com/ybxlO.jpg
 
+// imagem da lâmpada apagada
+// http://i.stack.imgur.com/b983w.jpg
+
 //imagem da lâmpada quebrada
 // http://i.stack.imgur.com/MRjsF.jpg
+
+const botao = document.querySelector('button.btn')
+botao.addEventListener('click', function(){
+  console.log("clicou!!!")
+})
