d27d0eb75d67565af71fcbc13386fb9abd0bfd4b
Jademir de Moura <jademirf@gmail.com>
2021-10-08 21:02:25 -0300

    finalizando com a troca da lampada funcionando

--- js/global.js
+++ js/global.js
@@ -7,14 +7,24 @@
 //imagem da lâmpada quebrada
 // http://i.stack.imgur.com/MRjsF.jpg
 
+let cont = 0
 const image = document.querySelector('img[alt="lampada"]')
 const botao = document.querySelector('button.btn')
 botao.addEventListener('click', function(){
   if(botao.textContent == "apagar") {
     botao.textContent = "acender"
     image.src = "http://i.stack.imgur.com/b983w.jpg"
+  } else if(botao.textContent == "trocar lampada") {
+    cont = 0
+    botao.textContent = "acender"
+    image.src = "http://i.stack.imgur.com/b983w.jpg"
   } else {
     image.src = "http://i.stack.imgur.com/ybxlO.jpg"
     botao.textContent = "apagar"
+    cont++
+  }
+  if (cont === 5) {
+    botao.textContent = "trocar lampada"
+    image.src = "http://i.stack.imgur.com/MRjsF.jpg"
   }
 })
